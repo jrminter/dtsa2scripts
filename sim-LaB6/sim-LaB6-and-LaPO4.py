61691dc7c4ab6ef38c31a144dd7b698cdedf0271
John Minter <jrminter@gmail.com>
2021-06-07 16:35:40 -0400

    added materials

--- sim-LaB6/sim-LaB6-and-LaPO4.py
+++ sim-LaB6/sim-LaB6-and-LaPO4.py
@@ -3,22 +3,21 @@
 """
 DTSA-II Script - J. R. Minter
 
-sim-nist-k961.py
+sim-LaB6-and-LaPO4.py
 
-A reproducible script to NIST K961 glass spectrum
+A reproducible script to simulate a LaB6 and LaPO4 spectra
 
   Date      Who   Ver    Comment
 ----------  ---  -----   ------------------------------------------
-2021-02-26  JRM  0.0.1   Initial test
+2021-04-29  JRM  0.0.1   Initial test
                          For 10,000 traj
-                         This script required 56.903 min
+                         This script required min
                          Elapse: 0:56:54.1
-                         Simulate NIST K961 glass spectrum
-                         Composition from Dale Newbury
+
 
 """
 
-__revision__ = "$Id: sim-nist-k961.py John R. Minter $"
+__revision__ = "$Id: sim-LaB6-and-LaPO4.py John R. Minter $"
 __version__ = "0.0.1"
 
 import sys
@@ -59,7 +58,7 @@ def defineMat(elms,qty,name,density=None):
 
 homDir = os.environ['HOME']
 homDir = homDir.replace('\\','/')
-wrkDir = homDir + "/Documents/git/dtsa2Scripts/sim-nist-k961/"
+wrkDir = homDir + "/Documents/git/dtsa2Scripts/sim-LaB6/"
 e0      = 20.0
 c_thick = 0.020 # 20 nm
 n_traj  = 10000
@@ -68,20 +67,45 @@ det     = findDetector("Oxford p4 05eV 4K")
 ctg     = material("C", 1.8)
 
 
-k961 = defineMat((  "O",  "Na",  "Mg",  "Al",  "Si",   "P", "K"  ,  "Ca", "Ti",  "Mn", "Fe"   ),
-	             (0.470, 0.030, 0.030, 0.058, 0.299, 0.002, 0.025, 0.036, 0.012, 0.003, 0.035 ),
-	              "K-961", 3.0)
+lab6  = defineMat(("La"   ,  "B",    ),
+	              (0.68168, 0.31832, ),
+	               "LaB6", 4.72)
+
+lapo4 = defineMat(("La"   ,     "P",  "O"    ),
+	              (0.59393, 0.13243,  0.27364,),
+	              "LaPO4", 4.93)
+
 
 # compute spectrum
-spc_k961 = mc3.coatedSubstrate(ctg, c_thick, k961, det, e0, True, n_traj, dose, True, True, {})
+spc_lab6 = mc3.coatedSubstrate(ctg, c_thick, lab6, det, e0, True, n_traj, dose, True, True, {})
+spc_lab6.rename("LaB6")
+# spc_lab6.setAsStandard(lab6)
+spc_lab6.display()
 
-spc_k961.rename("K-961")
-spc_k961.setAsStandard(k961)
-spc_k961.display()
+sName = "20-nm-C-on-LaB6-%g-kV" % (e0)
+spc_lab6.rename(sName)
+spc_lab6.display()
 
-sName = "20-nm-C-on-K961-glass-at-%g-kV" % (e0)
-spc_k961.rename(sName)
-spc_k961.display()
+end = time.time()
+delta = (end-start)/60
+msg = "This script required %.3f min" % delta
+print msg
+if(delta > 60):
+    delta = delta/60
+    msg = "...or %.3f hr" % delta
+    print msg
+
+
+start = time.time()
+
+spc_lapo4 = mc3.coatedSubstrate(ctg, c_thick, lapo4, det, e0, True, n_traj, dose, True, True, {})
+spc_lapo4.rename("LaPO4")
+# spc_lapo4.setAsStandard(lapo4)
+spc_lapo4.display()
+
+sName = "20-nm-C-on-LaPO4-%g-kV" % (e0)
+spc_lapo4.rename(sName)
+spc_lapo4.display()
 
 end = time.time()
 delta = (end-start)/60
