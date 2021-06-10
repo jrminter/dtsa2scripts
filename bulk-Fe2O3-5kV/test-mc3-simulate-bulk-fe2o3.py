ddb5c9c7cb18777a45b84ddac17d43948c350f50
John Minter <jrminter@gmail.com>
2021-06-10 17:01:54 -0400

    added Fe2O3 5kV

--- bulk-Fe2O3-5kV/test-mc3-simulate-bulk-fe2o3.py
+++ bulk-Fe2O3-5kV/test-mc3-simulate-bulk-fe2o3.py
@@ -1,28 +1,33 @@
-
+# test-mc3-simulate-fe2o3
+#
+# 2021-06-10 J.R. Minter
+#
 import os, shutil
 import dtsa2.mcSimulate3 as mc3
 
 start = time.time()
 
-homDir = os.environ['HOME']
-homDir = homDir.replace('\\','/')
-wrkDir = homDir + "/Documents/git/dtsa2Scripts/utility"
-outDir = homDir + "/Documents/git/dtsa2Scripts/utility/output/"
-rptDir = wrkDir + '/test-mc3-simulate-Fe2O3 Results/'
+homDir   = os.environ['HOME']
+homDir   = homDir.replace('\\','/')
+# store the results in the outDir directory (a place for spectra...)
+outDir   = homDir + "/Documents/git/dtsa2Scripts/utility/output/"
 
+bSaveSpc = True
 DataManager.clearSpectrumList()
 
-e0  = 20.0
-det = findDetector("Probe")
-mat = material("Fe2O3",5.0)
 
-spc = mc3.simulate(mat, det, e0, 120.0, True, 1000, True, True, {})
+e0    = 5.0
+nTraj = 10000
+det   = findDetector("Oxford p4 05eV 4K")
+mat   = material("Fe2O3",5.0)
+
+spc = mc3.simulate(mat, det, e0, 120.0, True, nTraj, True, True, {})
 sName = "Bulk Fe2O3"
 spc.rename(sName)
 spc.display()
+fi = outDir + "/Fe2O3-%g-kV-%g-Traj.msa" % (e0, nTraj)
+spc.save(fi)
 
-shutil.rmtree(rptDir)
-print "Done!"
 
 end = time.time()
 delta = (end-start)/60
