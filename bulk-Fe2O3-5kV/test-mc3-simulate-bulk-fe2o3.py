e8a872220677a41b998219d0cb5807ce9aa2b69c
John Minter <jrminter@gmail.com>
2021-06-10 19:39:29 -0400

    added Fe2O3 sim at 5 and 20kV

--- bulk-Fe2O3-5kV/test-mc3-simulate-bulk-fe2o3.py
+++ bulk-Fe2O3-5kV/test-mc3-simulate-bulk-fe2o3.py
@@ -8,20 +8,21 @@ homDir = os.environ['HOME']
 homDir = homDir.replace('\\','/')
 wrkDir = homDir + "/Documents/git/dtsa2Scripts/utility"
 outDir = homDir + "/Documents/git/dtsa2Scripts/utility/output/"
-rptDir = wrkDir + '/test-mc3-simulate-Fe2O3 Results/'
+rptDir = wrkDir + '/test-mc3-simulate-Fe2O3/'
 
 DataManager.clearSpectrumList()
 
-e0  = 20.0
-det = findDetector("Probe")
-mat = material("Fe2O3",5.0)
+e0    = 20.0
+nTraj = 10000
+det   = findDetector("Oxford p4 05eV 4K")
+mat   = material("Fe2O3",5.0)
 
-spc = mc3.simulate(mat, det, e0, 120.0, True, 1000, True, True, {})
+
+spc = mc3.simulate(mat, det, e0, 120.0, True, nTraj, True, True, {})
 sName = "Bulk Fe2O3"
 spc.rename(sName)
 spc.display()
 
-shutil.rmtree(rptDir)
 print "Done!"
 
 end = time.time()
