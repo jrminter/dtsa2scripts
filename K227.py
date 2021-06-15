b01ceea91bb29d043600ff1e9ce0a0756f06ff71
John Minter <jrminter@gmail.com>
2021-06-15 10:18:27 -0400

    better simK227

--- K227.py
+++ K227.py
@@ -16,3 +16,17 @@ for xrt in xrts:
 		if std.containsElement(elm):
 			stdMac=mac.computeWithUncertaintyEstimate(std,xrt)
 			print "%s\t%f\t%f\t%f\t%f" % (std,  std.weightFraction(elm,False), xpp.u_chi(std,mat,xrt,props), stdMac.doubleValue(),stdMac.uncertainty())
+
+"""
+2021-06-15  J. R. Minter
+----------
+
+Running /Users/jrminter/Documents/git/dtsa2scripts/K227/K227.py
+Si K-L3 136.550064   11.874185
+O K-L3  985.465932  167.903408
+Pb L3-M5  8.156100    0.783112
+Pb M5-N7 86.380349    6.496248
+
+Elapse: 0:00:00.4
+"""
+
